ab8c3fedc46c1f99beb734167b31a89dd9873702
Pavan543210 <135861719+Pavan543210@users.noreply.github.com>
2024-07-08 01:23:10 +0530

    Update NewsWidget.js

--- src/components/NewsWidget.js
+++ src/components/NewsWidget.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-axios.defaults.httpAgent.protocol = 'http:';
-axios.defaults.httpsAgent.protocol = 'https:';
 
 const NewsWidget = ({ defaultLocation = 'India' }) => {
   const [location, setLocation] = useState(defaultLocation);
